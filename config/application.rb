7016de88b37cfdcab339c9cdbcc10523d61ed6c2
Conor <conoreflanagan@gmail.com>
2013-06-14 09:31:39 -0700

    Added Amazon YML

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,11 @@ module AmazonToy
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.before_configuration do
+  env_file = File.join(Rails.root, 'config', 'amazon_api.yml')
+  YAML.load(File.open(env_file)).each do |key, value|
+    ENV[key.to_s] = value
+  end if File.exists?(env_file)
+end
   end
 end
--- config/initializers/amazon.rb
+++ config/initializers/amazon.rb
@@ -1,5 +1,7 @@
+# ENV = YAML.load_file("#{::Rails.root}/config/amazon_api.yml")[::Rails.env]
 Amazon::Ecs.options = {
   :associate_tag => ENV['associatetag'],
   :AWS_access_key_id => ENV['accesskeyid'],
   :AWS_secret_key => ENV['secretkey']
 }
+# puts AMAZON_CONFIG['associatetag'] + "___________________________"
